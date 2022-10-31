485c31934506c518e56357cfeb7a61f73abf415b
Olayinka <theolayinkayodele@gmail.com>
2022-10-31 14:42:32 +0100

    Initializes index files

---
+++ index.js
@@ -0,0 +1,13 @@
+let basic = document.querySelector('.basic');
+let standard = document.querySelector('.standard');
+let premium = document.querySelector('.premium');
+
+
+let arr = [basic, standard, premium];
+
+
+
+function makeActive(element) {
+    element.classList.toggle('active');
+    alert('Ensure to fucking unclick before you click another 😭')
+}
